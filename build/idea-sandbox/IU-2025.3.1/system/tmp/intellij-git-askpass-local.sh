31f8ba738e1a81e84fa1ae49402c287f4c3b9451
Dragan Prpa <prpa.dev@gmail.com>
2026-04-26 05:27:35 +0200

    oops v3

---
+++ build/idea-sandbox/IU-2025.3.1/system/tmp/intellij-git-askpass-local.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"/Users/draganprpa/.gradle/caches/9.2.1/transforms/62476e296f861c9feb2828e10ec43fb8/transformed/idea-2025.3.1-aarch64/jbr/Contents/Home/bin/java" -cp "/Users/draganprpa/.gradle/caches/9.2.1/transforms/62476e296f861c9feb2828e10ec43fb8/transformed/idea-2025.3.1-aarch64/plugins/vcs-git/lib/git4idea-rt.jar:/Users/draganprpa/.gradle/caches/9.2.1/transforms/62476e296f861c9feb2828e10ec43fb8/transformed/idea-2025.3.1-aarch64/lib/externalProcess-rt.jar" git4idea.http.GitAskPassApp "$@"
